45312658f63ef3ea6fb7c37e1910d961f126c8d7
SzombatiAttila <szombati.attila96@gmail.com>
2018-11-13 19:16:01 +0000

    valami

--- masik.py
+++ masik.py
@@ -1,3 +1,9 @@
-with open('tmp.csv','r') as f:
-	last_line = f.readlines()[-1]
-	print(last_line)
+
+def read_data():
+	with open('tmp.csv','r') as f:
+		lastline = ""
+		for line in f:
+			lastline = line
+		print(lastline)
+
+read_data()
